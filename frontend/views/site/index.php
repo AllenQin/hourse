e4ed95cabd3a043dc8e3d2f9813d8085cc475421
秦海龙 <allenqinhai@gmail.com>
2017-04-04 10:04:08 +0800

    update index view

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,52 +2,216 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = '房直播 - 首页';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+<div class="scrollblock">
+      <section id="feature">
+        <div class="container">
+          <div class="row">
+            <div class="span12">
+              <article>
+                <p>筛选海外最适宜居住的房产</p>
+                <p>推荐最适合您的投资组合</p>
+                <p>筛选最新海外房地产信息动态</p>
+              </article>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            <!-- ./span12 -->
+          </div>
+          <!-- .row -->
         </div>
-
+        <!-- ./container -->
+      </section>
     </div>
-</div>
+    <hr>
+    <!--******************** Portfolio Section ********************-->
+    <section id="portfolio" class="single-page scrollblock">
+      <div class="container">
+        <div class="align"><i class="icon-desktop-circled"></i></div>
+        <h1 id="folio-headline">投资组合</h1>
+        
+        <div class="row">
+          <div class="span4">
+            <div class="mask2"> <a href="/img/portfolio-01.jpg" rel="prettyPhoto"><img src="/img/portfolio-01.jpg" alt=""></a> </div>
+            <div class="inside">
+              <hgroup>
+                <h2>泰国xxx房地产</h2>
+              </hgroup>
+              <div class="entry-content">
+                <p>项目介绍</p>
+                <a class="more-link" href="#">详细</a> </div>
+            </div>
+            <!-- /.inside -->
+          </div>
+          <!-- /.span4 -->
+          <div class="span4">
+            <div class="mask2"> <a href="/img/portfolio-02.jpg" rel="prettyPhoto"><img src="/img/portfolio-02.jpg" alt=""></a> </div>
+            <div class="inside">
+              <hgroup>
+                <h2>新西兰</h2>
+              </hgroup>
+              <div class="entry-content">
+                <p>项目介绍</p>
+                <a class="more-link" href="#">详细</a> </div>
+            </div>
+            <!-- /.inside -->
+          </div>
+          <!-- /.span4 -->
+          <div class="span4">
+            <div class="mask2"> <a href="/img/portfolio-03.jpg" rel="prettyPhoto"><img src="/img/portfolio-03.jpg" alt=""></a> </div>
+            <div class="inside">
+              <hgroup>
+                <h2>泰国xx房产项目2</h2>
+              </hgroup>
+              <div class="entry-content">
+                <p>项目介绍</p>
+                <a class="more-link" href="#">详细</a> </div>
+            </div>
+            <!-- /.inside -->
+          </div>
+          <!-- /.span4 -->
+        </div>
+      </div>
+      <!-- /.container -->
+    </section>
+    <hr>
+    <!--******************** Services Section ********************-->
+    <section id="services" class="single-page scrollblock">
+      <div class="container">
+        <div class="align"><i class="icon-cog-circled"></i></div>
+        <h1>服务</h1>
+        <!-- Four columns -->
+        <div class="row">
+          <div class="span3">
+            <div class="align"> <i class="icon-desktop sev_icon"></i> </div>
+            <h2>免费咨询</h2>
+            <p>输入订阅邮箱，免费接收最新海外房产动态</p>
+          </div>
+          <!-- /.span3 -->
+          <div class="span3">
+            <div class="align"> <i class="icon-vector sev_icon"></i> </div>
+            <h2>直播看房</h2>
+            <p>足不出户，帮您360度无死角观察现房，直播看房</p>
+          </div>
+          <!-- /.span3 -->
+          <div class="span3">
+            <div class="align"> <i class="icon-basket sev_icon"></i> </div>
+            <h2>投资顾问</h2>
+            <p>量身定制，推荐最优质资源，提供最优化投资方案</p>
+          </div>
+          <!-- /.span3 -->
+          <div class="span3">
+            <div class="align"> <i class="icon-mobile-1 sev_icon"></i> </div>
+            <h2>代理负责人</h2>
+            <p>负责中间一应繁琐环节，保障购房人信息资料安全，减少您的顾虑</p>
+          </div>
+          <!-- /.span3 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <hr>
+    <!--******************** Testimonials Section ********************-->
+    <section id="testimonials" class="single-page hidden-phone">
+      <div class="container">
+        <div class="row">
+          <div class="blockquote-wrapper">
+            <blockquote class="mega">
+              <div class="span4">
+                <p class="cite">云房团队:</p>
+              </div>
+              <div class="span8">
+                <p class="alignright">世界很大，您值得去看看！</p>
+              </div>
+            </blockquote>
+          </div>
+          <!-- /.blockquote-wrapper -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <hr>
+    <!--******************** News Section ********************-->
+    <section id="news" class="single-page scrollblock">
+      <div class="container">
+        <div class="align"><i class="icon-pencil-circled"></i></div>
+        <h1>资讯</h1>
+        <!-- Three columns -->
+        <div class="row">
+          <article class="span4 post"> <img class="img-news" src="/img/blog_img-01.jpg" alt="">
+            <div class="inside">
+              <p class="post-date"><i class="icon-calendar"></i>2017-03-03</p>
+              <h2>最新海外购房政策</h2>
+              <div class="entry-content">
+                <p>内容简介</p>
+                <a href="#" class="more-link">详细</a> </div>
+            </div>
+            <!-- /.inside -->
+          </article>
+          <!-- /.span4 -->
+          <article class="span4 post"> <img class="img-news" src="/img/blog_img-02.jpg" alt="">
+            <div class="inside">
+              <p class="post-date">2013-07-04</p>
+              <h2>泰国购买政策</h2>
+              <div class="entry-content">
+                <p>内容简介</p>
+                <a href="#" class="more-link">详细</a> </div>
+            </div>
+            <!-- /.inside -->
+          </article>
+          <!-- /.span4 -->
+          <article class="span4 post"> <img class="img-news" src="/img/blog_img-03.jpg" alt="">
+            <div class="inside">
+              <p class="post-date">2017-03-5</p>
+              <h2>新西兰房产</h2>
+              <div class="entry-content">
+                <p>内容介绍</p>
+                <a href="#" class="more-link">详细</a> </div>
+            </div>
+            <!-- /.inside -->
+          </article>
+          <!-- /.span4 -->
+        </div>
+        <!-- /.row -->
+        <a href="#" class="btn btn-large">更多</a> </div>
+      <!-- /.container -->
+    </section>
+    <hr>
+    <!--******************** Team Section ********************-->  
+    <!--******************** Contact Section ********************-->
+    <section id="contact" class="single-page scrollblock">
+      <div class="container">
+        <div class="align"><i class="icon-mail-2"></i></div>
+        <h1>留言</h1>
+        <div class="row">
+          <div class="span12">
+            <div class="cform" id="theme-form">
+              <form action="#" method="post" class="cform-form">
+                <div class="row">
+                  <div class="span6"> <span class="your-name">
+                    <input type="text" name="your-name" placeholder="姓名" class="cform-text" size="40" title="姓名">
+                    </span> </div>
+                  <div class="span6"> <span class="your-email">
+                    <input type="text" name="your-phone" placeholder="电话" class="cform-text" size="40" title="电话">
+                    </span> </div>
+                </div>
+                <div class="row">
+                  <div class="span12"> <span class="message">
+                    <textarea name="message" class="cform-textarea" cols="40" rows="10" title="drop us a line."></textarea>
+                    </span> </div>
+                </div>
+                <div>
+                  <input type="submit" value="发送留言" class="cform-submit pull-left">
+                </div>
+                <div class="cform-response-output"></div>
+              </form>
+            </div>
+          </div>
+          <!-- ./span12 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <hr>
